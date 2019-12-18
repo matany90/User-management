59e9a5052f406d68e3beb0d5827528aee39d3ead
MATAN\matan <matany90@gmail.com>
2019-12-19 01:05:53 +0200

    setup build client folder in server && setup server for prod

--- server/index.js
+++ server/index.js
@@ -13,5 +13,13 @@ app.use(bodyParser.json());
 
 require('./routes/usersRoutes')(app);
 
+//Handle Production
+if (process.env.NODE_ENV === 'production') {
+    //client static folder
+    app.use(express.static(__dirname, '/public/'));
+    //single page application
+    app.get('*', (req, res) => res.sendFile(__dirname, '/public/index.html'));
+}
+
 const PORT = process.env.PORT || 5000; // listen to Heroku's given port (prod), or take 5000 (dev)
 app.listen(PORT);
