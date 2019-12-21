085f7ef6985120cee0112874d2557a73e11fa0f4
MATAN\matan <matany90@gmail.com>
2019-12-21 02:25:42 +0200

    efactor endpoints - 1

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -10,30 +10,37 @@ const ERROR_ADD_USER = 'Error adding user!'
 module.exports = (app) => {
     /** FETCH USERS endpoint  **/
     app.get('/api/users', async (req, res) => {
+        let response;
         const snapshot = await db.collection('users').get()
         /* Adding doc-firestore-id to each element
         to filter correctly when deleting/editing */
         if (snapshot) {
             const users =  snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-            return res.send({ users, isSuccess: true })
+            response = { users, isSuccess: true };
         }
-        return res.send({ error: ERROR_MSG, isSuccess: false })
+        else {
+            response = { error: ERROR_MSG, isSuccess: false };
+        }
+
+        res.send(response)
     })
 
     /** ADD USER endpoint **/
     app.post('/api/users/addUser', async (req, res) => {
         const { name, email, phone } = req.body.user;
         //add user
-        const response = await db.collection('users').add({
+        const dbRes = await db.collection('users').add({
             name,
             email,
             phone
         })
         //pull new user
-        const doc = await db.collection('users').doc(response.id).get();
+        const doc = await db.collection('users').doc(dbRes.id).get();
         //check if added successfully
-        const isSuccess = doc.id === response.id;
-        isSuccess ? res.send({ user: {...doc.data(), id: doc.id }, isSuccess }) : res.send({ error: ERROR_ADD_USER, isSuccess })
+        const isSuccess = doc.id === dbRes.id;
+        const response = isSuccess ? { user: {...doc.data(), id: doc.id }, isSuccess } : { error: ERROR_ADD_USER, isSuccess }
+
+        res.send(response)
     })
 
     /** DELETE USER endpoint **/
@@ -43,7 +50,9 @@ module.exports = (app) => {
         await db.collection('users').doc(id).delete();
         //check if deleted successfully
         const doc = await db.collection('users').doc(id).get();
-        doc.exists ? res.send({ error: ERROR_MSG, isSuccess: false }) : res.send({ user: req.body.user, isSuccess: true })
+        const response =  doc.exists ? { error: ERROR_MSG, isSuccess: false } : { user: req.body.user, isSuccess: true };
+
+        res.send(response)
     })
 
     /** UPDATE USER endpoint **/
@@ -56,6 +65,8 @@ module.exports = (app) => {
         const updatedUser = doc.exists ? {...doc.data(), id: user.id }: {};
         //check if updated successfully
         const isSuccess = _.isEqual(updatedUser, user);
-        isSuccess ? res.send({ user: updatedUser, isSuccess }) : res.send({ error: ERROR_UPDATE_USER, isSuccess })
+        const response = isSuccess ? { user: updatedUser, isSuccess } : { error: ERROR_UPDATE_USER, isSuccess }
+
+        res.send(response);
     })
 }
